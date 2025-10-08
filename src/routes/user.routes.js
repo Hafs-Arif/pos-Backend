27caf6b0c133134ace277062ca7bc8641092e27e
Hafs <hafsarif99@gmmail.com>
2025-10-08 21:08:21 +0500

    Post Patched

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.get("/", getUsers);
 router.get("/:id", getUserById);
-router.post("/", createUser);
+router.post("/login", createUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 
